353639893430526c3ee0b06a7afe2ecb110b9f51
c.graumann <christian.graumann@haw-hamburg.de>
2019-10-12 15:51:16 +0200

    added sample algorithm runs

--- src/main/java/mvc/model/Main.java
+++ src/main/java/mvc/model/Main.java
@@ -1,6 +1,8 @@
 package mvc.model;
 
+import mvc.model.algorithm.AStar;
 import mvc.model.algorithm.Bfs;
+import mvc.model.algorithm.SucheMitEinheitlichenKosten;
 import mvc.model.algorithm.Tfs;
 import mvc.model.problem.Problem;
 
@@ -8,9 +10,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // 1. Feld setzten
-        Problem problem = new Problem(10);
 
+        // 1. Feld setzten
+        Problem problem = new Problem(50);
+        /*
         // Felder blocken
         problem.blockNode(5);
         problem.blockNode(6);
@@ -27,11 +30,11 @@ public class Main {
         problem.blockNode(51);
 
         // Feld ausgeben
-        problem.drawField();
+        //problem.drawField();
 
         // Algorithmus durchlaufen lassen
         Bfs bfs = new Bfs();
-        bfs.calculate(problem, 55, 18);
+        bfs.calculate(problem, 1520, 18);
 
         // Algorithmus in das Feld eintragen
         problem.paintAlgorithmInToField(bfs);
@@ -39,7 +42,7 @@ public class Main {
         // Feld ausgeben
         System.out.println();
         problem.drawField();
-
+        /*
         // Feld leeren vom Algortihmus
         System.out.println();
         problem.clearFieldFromAlgorithm();
@@ -49,10 +52,55 @@ public class Main {
         problem.drawField();
 
         Tfs tfs = new Tfs();
-        tfs.calculate(problem, 55, 18);
+        tfs.calculate(problem, 1520, 18);
         problem.paintAlgorithmInToField(tfs);
         System.out.println();
         problem.drawField();
+        */
 
+        System.out.println();
+        problem.clearFieldFromAlgorithm();
+
+        // Algorithmus durchlaufen lassen
+        SucheMitEinheitlichenKosten smek = new SucheMitEinheitlichenKosten();
+        smek.calculate(problem, 1520, 18);
+
+        // Algorithmus in das Feld eintragen
+        problem.paintAlgorithmInToField(smek);
+
+        // Feld ausgeben
+        System.out.println();
+        problem.drawField();
+
+        System.out.println();
+        problem.clearFieldFromAlgorithm();
+
+
+        problem.blockNode(67);
+        problem.blockNode(68);
+        problem.blockNode(69);
+        problem.blockNode(70);
+        problem.blockNode(71);
+        problem.blockNode(121);
+        problem.blockNode(122);
+        problem.blockNode(123);
+        problem.blockNode(124);
+        problem.blockNode(125);
+        problem.blockNode(126);
+        problem.blockNode(127);
+        problem.blockNode(128);
+
+
+        // Algorithmus durchlaufen lassen
+        AStar aStar = new AStar();
+        aStar.calculate(problem, 1520, 22);
+
+
+        // Algorithmus in das Feld eintragen
+        problem.paintAlgorithmInToField(aStar);
+
+        // Feld ausgeben
+        System.out.println();
+        problem.drawField();
     }
 }
