f9cafaeca3a34cd4f75c43a88223d4aafa330fe8
c.graumann <christian.graumann@haw-hamburg.de>
2019-10-16 12:10:42 +0200

    added mehtods to calc uninformed and informed refactoring

--- src/main/java/mvc/model/PathFinding.java
+++ src/main/java/mvc/model/PathFinding.java
@@ -1,262 +1,181 @@
 package mvc.model;
 
 import mvc.model.algorithm.SearchAlgorithm;
-import mvc.model.algorithm.informed.AStar;
 import mvc.model.algorithm.informed.InformedAlgorithm;
 import mvc.model.algorithm.informed.heurisitc.Heuristic;
+import mvc.model.algorithm.uninformed.UninformedAlgorithm;
 import mvc.model.field.Field;
 import mvc.model.field.Node;
 import org.reflections.Reflections;
 import org.reflections.scanners.SubTypesScanner;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class PathFinding {
 
 
-    private Map<String, String> algorithmMap;
-    private Map<String, String> heuristicMap;
+    private Map<String, String> uninformedMap;
     private Map<String, String> informedMap;
-    private List<Integer> openList;
-    private List<Integer> closeList;
-    private List<Integer> path;
+    private Map<String, String> heuristicMap;
+
+    private SearchAlgorithm searchAlgorithm;
+
 
     public PathFinding() {
-        algorithmMap = new HashMap<String, String>();
-        heuristicMap = new HashMap<String, String>();
+        uninformedMap = new HashMap<>();
+        heuristicMap = new HashMap<>();
         informedMap = new HashMap<>();
-        openList = new ArrayList<>();
-        closeList = new ArrayList<>();
-        path = new ArrayList<>();
-        initAlgorithms();
-        initHeuristic();
+        initUninformedAlgorithms();
         initInformedAlgorithm();
+        initHeuristic();
     }
 
 
-    public void calc(int fieldColumns, Set<Integer> blockSet, String algortihmName, int source, int target){
-        System.out.println("Führe Pathfinding aus: FieldRows: " + fieldColumns + " Algorithmus: " + algortihmName + " Source: " + source + " Target: " + target);
-        Field field = new Field(fieldColumns);
-        openList = new ArrayList<>();
-        closeList = new ArrayList<>();
-        path = new ArrayList<>();
+    public void uninformedCalc(int fieldColumns, Set<Integer> blockSet, String uninformedAlgorithmName, int source, int target) throws Exception {
+        System.out.println("Pathfinding uninformiert: FieldRows: " + fieldColumns + " Algorithmus: "
+                + uninformedAlgorithmName + " Source: " + source + " Target: " + target);
 
+        if (!uninformedMap.containsKey(uninformedAlgorithmName))
+            throw new IllegalArgumentException("not correct uninformied algortihm");
+
+        Field field = new Field(fieldColumns);
         field.blockNode(blockSet);
 
-        SearchAlgorithm algorithm = getAlgorithm(algortihmName, field, source, target);
-        algorithm.calculate();
-
-        for(Node node: algorithm.getOpenList()){
-            openList.add(node.getZustand());
-        }
-
-        for(Node node: algorithm.getCloseList()){
-            closeList.add(node.getZustand());
-        }
-
-        for(Node node: algorithm.getPath()){
-            path.add(node.getZustand());
-        }
-
+        searchAlgorithm = loadUnInformedAlgorithm(uninformedAlgorithmName, field, source, target);
+        searchAlgorithm.calculate();
 
     }
 
-    // comment
-    public void calc(int fieldColumns, Set<Integer> blockSet, String algortihmName, String aStarHeuristic, int source, int target){
 
-        System.out.println("Führe Pathfinding aus: FieldRows: " + fieldColumns + " Algorithmus: " + algortihmName + " Source: " + source + " Target: " + target);
+    public void informedCalc(int fieldColumns, Set<Integer> blockSet, String informedAlgorithmName, String heuristicName, int source, int target) throws Exception {
+        System.out.println("Pathfinding informiert: FieldRows: " + fieldColumns + " Algorithmus: "
+                + informedAlgorithmName + " Heuristik: " + heuristicName + " Source: " + source + " Target: " + target);
+
+        if (!informedMap.containsKey(informedAlgorithmName))
+            throw new IllegalArgumentException("not correct informed algorithm");
+        if (!heuristicMap.containsKey(heuristicName))
+            throw new IllegalArgumentException("not correct heuristic");
+
         Field field = new Field(fieldColumns);
-        openList = new ArrayList<>();
-        closeList = new ArrayList<>();
-        path = new ArrayList<>();
-
         field.blockNode(blockSet);
 
-        SearchAlgorithm algorithm;
-
-        if(aStarHeuristic != null && algortihmName.equals("AStar")){
-
-            algorithm = new AStar(field, source, target, getHeuristic(aStarHeuristic, field.getField()[target], fieldColumns));
-            algorithm.calculate();
-
-            for(Node node: algorithm.getOpenList()){
-                openList.add(node.getZustand());
-            }
-
-            for(Node node: algorithm.getCloseList()){
-                closeList.add(node.getZustand());
-            }
-
-            for(Node node: algorithm.getPath()){
-                path.add(node.getZustand());
-            }
-
-        } else {
-
-            algorithm = getAlgorithm(algortihmName, field, source, target);
-            algorithm.calculate();
-
-            for(Node node: algorithm.getOpenList()){
-                openList.add(node.getZustand());
-            }
-
-            for(Node node: algorithm.getCloseList()){
-                closeList.add(node.getZustand());
-            }
-
-            for(Node node: algorithm.getPath()){
-                path.add(node.getZustand());
-            }
-        }
-
-
-
-
-
-
+        searchAlgorithm = loadInformedAlgorithm(informedAlgorithmName, heuristicName, field, source, target);
+        searchAlgorithm.calculate();
 
     }
 
+
+    private void initUninformedAlgorithms() {
+
+        Set<Class<? extends UninformedAlgorithm>> searchAlgortihms = new Reflections("mvc.model.algorithm",
+                new SubTypesScanner(false))
+                .getSubTypesOf(UninformedAlgorithm.class);
+
+        for (Class object : searchAlgortihms) {
+            if (!Modifier.isAbstract(object.getModifiers())) {
+                uninformedMap.put(object.getSimpleName(), object.getName());
+            }
+        }
+    }
+
+
+    private void initHeuristic() {
+
+        Set<Class<? extends Heuristic>> informedAlgorithms = new Reflections("mvc.model.algorithm",
+                new SubTypesScanner(false))
+                .getSubTypesOf(Heuristic.class);
+
+        for (Class object : informedAlgorithms) {
+            if (!Modifier.isAbstract(object.getModifiers())) {
+                heuristicMap.put(object.getSimpleName(), object.getName());
+            }
+        }
+    }
+
+    private void initInformedAlgorithm() {
+
+        Set<Class<? extends InformedAlgorithm>> heuristics = new Reflections("mvc.model.algorithm",
+                new SubTypesScanner(false))
+                .getSubTypesOf(InformedAlgorithm.class);
+
+        for (Class object : heuristics) {
+            if (!Modifier.isAbstract(object.getModifiers())) {
+                informedMap.put(object.getSimpleName(), object.getName());
+            }
+        }
+    }
+
+
+    private UninformedAlgorithm loadUnInformedAlgorithm(String algorithmName, Field field, int source, int target) throws Exception {
+
+        return (UninformedAlgorithm) Class.forName(uninformedMap.get(algorithmName))
+                .getConstructors()[0].newInstance(field, source, target);
+
+    }
+
+    private InformedAlgorithm loadInformedAlgorithm(String algorithmName, String heuristicName, Field field, int source, int target) throws Exception {
+
+        Heuristic heuristic = loadHeuristic(heuristicName, field.getField()[target], field.getColumns());
+
+        return (InformedAlgorithm) Class.forName(informedMap.get(algorithmName))
+                .getConstructors()[0].newInstance(field, source, target, heuristic);
+
+    }
+
+    private Heuristic loadHeuristic(String heuristicName, Node target, int columns) throws Exception {
+
+        return (Heuristic) Class.forName(heuristicMap.get(heuristicName))
+                .getConstructors()[0].newInstance(target, columns);
+
+    }
+
+    public List<String> getUninformedAlgorithm() {
+        return new ArrayList<>(uninformedMap.keySet());
+    }
+
+    public List<String> loadHeuristic() {
+        return new ArrayList<>(heuristicMap.keySet());
+    }
+
+    public List<String> loadInformedAlgorithm() {
+        return new ArrayList<>(informedMap.keySet());
+    }
 
     public List<Integer> getOpenList() {
-        return openList;
+        if(searchAlgorithm == null) return new ArrayList<>();
+        return searchAlgorithm.getOpenList().stream().map(Node::getZustand).collect(Collectors.toList());
     }
 
     public List<Integer> getCloseList() {
-        return closeList;
+        if(searchAlgorithm == null) return new ArrayList<>();
+        return searchAlgorithm.getCloseList().stream().map(Node::getZustand).collect(Collectors.toList());
     }
 
     public List<Integer> getPath() {
-        return path;
-    }
-
-    private void initAlgorithms()  {
-
-        Reflections reflections = new Reflections("mvc.model.algorithm", new SubTypesScanner(false));
-        Set<Class<? extends SearchAlgorithm>> searchAlgortihms = reflections.getSubTypesOf(SearchAlgorithm.class);
-
-        for(Class object: searchAlgortihms){
-            algorithmMap.put(object.getSimpleName(), object.getName());
-        }
-
-        System.out.println("Verfügbare Algorithmen: " + algorithmMap.keySet());
-    }
-
-    private void initHeuristic()  {
-
-        Reflections reflections = new Reflections("mvc.model.algorithm", new SubTypesScanner(false));
-        Set<Class<? extends Heuristic>> informedAlgorithms = reflections.getSubTypesOf(Heuristic.class);
-
-        for(Class object: informedAlgorithms){
-            heuristicMap.put(object.getSimpleName(), object.getName());
-        }
-
-        System.out.println("Verfügbare Heuristiken: " + heuristicMap.keySet());
-    }
-
-    private void initInformedAlgorithm()  {
-
-        Reflections reflections = new Reflections("mvc.model.algorithm", new SubTypesScanner(false));
-        Set<Class<? extends InformedAlgorithm>> heuristics = reflections.getSubTypesOf(InformedAlgorithm.class);
-
-        for(Class object: heuristics){
-            informedMap.put(object.getSimpleName(), object.getName());
-        }
-
-        System.out.println("Verfügbare informierte Algorithmen: " + informedMap.keySet());
-    }
-    
-    public List<String> avaiableAlgortihm(){
-        List<String> result = new ArrayList<>();
-        result.addAll(algorithmMap.keySet());
-        System.out.println("Übergebe Algorithmen: " + result);
-        return result;
-    }
-
-    public List<String> avaiableHeuristic(){
-        List<String> result = new ArrayList<>();
-        result.addAll(heuristicMap.keySet());
-        System.out.println("Übergebe Heuristiken: " + result);
-        return result;
-    }
-
-    public List<String> avaiableInformedAlgorithm(){
-        List<String> result = new ArrayList<>();
-        result.addAll(informedMap.keySet());
-        System.out.println("Übergebe informierte Algorithmen: " + result);
-        return result;
-    }
-
-    private SearchAlgorithm getAlgorithm(String algorithmName, Field field, int source, int target){
-
-
-        SearchAlgorithm algorithm = null;
-
-        try {
-            Class<?> clazz = Class.forName(algorithmMap.get(algorithmName));
-
-            Constructor<?> ctor = clazz.getConstructors()[0];
-
-            algorithm = (SearchAlgorithm) ctor.newInstance( field, source, target);
-
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        return algorithm;
-
-    }
-
-    private Heuristic getHeuristic(String heuristicName, Node target, int columns){
-
-
-        Heuristic heuristic = null;
-
-        try {
-            Class<?> clazz = Class.forName(heuristicMap.get(heuristicName));
-
-            Constructor<?> ctor = clazz.getConstructors()[0];
-
-            heuristic = (Heuristic) ctor.newInstance(target, columns);
-
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        return heuristic;
-
+        if(searchAlgorithm == null) return new ArrayList<>();
+        return searchAlgorithm.getPath().stream().map(Node::getZustand).collect(Collectors.toList());
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         PathFinding pathFinding = new PathFinding();
 
-        System.out.println(pathFinding.avaiableAlgortihm());
-        System.out.println(pathFinding.avaiableHeuristic());
-        System.out.println(pathFinding.avaiableInformedAlgorithm());
+        pathFinding.uninformedCalc(15, null, "BreadthFirst", 1, 10);
+        System.out.println(pathFinding.getOpenList());
 
-        pathFinding.calc(15,null, "DepthFirst", null, 1, 10);
-        pathFinding.calc(15,null, "UniformCost", null, 1, 10);
-        pathFinding.calc(15,null, "AStar", null, 1, 10);
-        pathFinding.calc(15,null, "BreadthFirst", null, 1, 10);
-        pathFinding.calc(15,null, "AStar", "EuclideanDistance", 1, 10);
+        pathFinding.uninformedCalc(15, null, "DepthFirst", 1, 10);
+        System.out.println(pathFinding.getOpenList());
 
+        pathFinding.uninformedCalc(15, null, "BreadthFirst", 1, 10);
+        System.out.println(pathFinding.getOpenList());
 
+        pathFinding.informedCalc(15, null, "AStar", "ManhattenDistance", 1, 10);
+        System.out.println(pathFinding.getOpenList());
 
+        pathFinding.informedCalc(15, null, "AStar", "EuclideanDistance", 1, 10);
+        System.out.println(pathFinding.getOpenList());
 
     }
 }
