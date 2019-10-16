a3cd527da0dc6d1e79b64f5541db06352af55517
c.graumann <christian.graumann@haw-hamburg.de>
2019-10-16 14:21:25 +0200

    added placeholder for IDAStar

---
+++ src/main/java/mvc/model/algorithm/informed/IDAStar.java
@@ -0,0 +1,17 @@
+package mvc.model.algorithm.informed;
+
+import mvc.model.algorithm.informed.heurisitc.Heuristic;
+import mvc.model.field.Field;
+
+public class IDAStar extends InformedAlgorithm {
+
+    public IDAStar(Field field, int source, int target, Heuristic heuristic) {
+        super(field, source, target, heuristic);
+    }
+
+    @Override
+    public void calculate() {
+
+    }
+
+}
